37c06e6a3ff4624f6b2f1554d7320cf86a4e5f8f
CBedzz <swizzlestack@gmail.com>
2020-06-22 00:20:13 -0600

    rendering lists

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 
 class App extends React.Component {
 	render() {
 		return (
-			<React.Fragment>
-				<div>Hello World!</div>
-			</React.Fragment>
+			<div className='container'>
+				<Popular />
+			</div>
 		)
 	}
 }
